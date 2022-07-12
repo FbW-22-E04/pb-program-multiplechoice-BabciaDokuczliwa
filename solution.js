4df56d9fdb9adec375682e653cc6f1b6f4126fc6
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-07-12 11:48:18 +0200

    ex muliplechoiceProgram

--- solution.js
+++ solution.js
@@ -1,1 +1,22 @@
 // write your code here
+
+let color = `black`;
+switch(color) {
+    case `red`:
+        console.log('Q1: Red`s a great color on you.');
+     break;  
+
+    case `blue`:
+        console.log('Q1: Blue? Great choice.');
+     break;   
+
+    case `green`:
+        console.log('Q1: Ok. Green it is.');
+     break; 
+      case `yellow`:
+        console.log('Q1: Not what I would have picked, but yellow is fine.');
+     break;  
+ default:
+    console.log('Q1: The aim of the game is to pick a color.');
+    
+}
